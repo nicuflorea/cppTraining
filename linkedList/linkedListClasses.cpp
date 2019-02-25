403e4ade6b855fdbd1b320285c81f98e45eb61c7
florea <nicusor.florea@tttech.com>
2019-02-25 10:26:54 +0200

    AEROASIC-1234: Overloaded remove node function.

---
+++ linkedList/linkedListClasses.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+
+class node{
+public:
+   int iData;
+   node* next;
+   node(){
+      iData = 0;
+      next = NULL;
+      cout << "Initializing data ..." << endl;}
+};
+
+class linkedList{
+public:
+    node listHead, listTail;
+private:
+    static int nodeCount;
+public:
+
+    linkedList(){
+       nodeCount = 0;
+       // all nodes point to the same location
+       listHead.next = &listTail;
+       listTail.next = &listHead;
+       }
+
+    void addItem(int i);
+    void removeItem();
+    void removeItem(int i);
+    void printList();
+};
+
+int linkedList::nodeCount = 0;
+
+void linkedList::addItem(int i){
+   nodeCount = nodeCount + 1;
+   // Create new node
+   node* newNode = new node;
+   newNode->iData = i;
+   newNode->next = listTail.next;
+   listTail.next = newNode;
+   // Keep the head pointer to NULL
+   listHead.next = NULL;
+};
+
+void linkedList::removeItem(){
+   if (nodeCount == 0){cout << "List is empty ..." <<endl;}
+   else{
+
+      node* current;
+      current = listTail.next;
+      listTail.next = current -> next;
+      delete current;}
+   };
+
+void linkedList::removeItem(int i){
+   node* current;
+   current = &listTail;
+   if (nodeCount == 0){cout << "List is empty ..." <<endl;return;}
+   else{
+      while (current != &listHead){
+         // if value was found
+         if (current -> next -> iData == i){
+            cout<< "Found match in list ... " << endl;
+            delete current -> next;
+            current -> next = current->next->next;
+            nodeCount = nodeCount - 1;
+            return;
+         }
+         else{
+            current = current -> next;
+         }
+      }
+      cout <<"Value not found ... "<<endl;
+      return;
+   }};
+
+
+void linkedList::printList(){
+   node* current;
+   current = listTail.next;
+   cout << "Number of items in the list is ..." << nodeCount << endl;
+   while (current != &listHead){
+      cout << "Data is ..." << current->iData << endl;
+      current = current->next;
+   }
+};
--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -1,57 +1,4 @@
-#include <iostream>
-using namespace std;
-class node{
-public:
-    int iData;
-    node* next;
-
-    node(){
-       iData = 0;
-       next = NULL;
-       cout << "Initializing data ..." << endl;}
-
-};
-
-class linkedList{
-public:
-    node listHead, listTail, listTemp;
-private:
-    static int nodeCount;
-public:
-
-    linkedList(){
-        nodeCount = 0;
-        // all nodes point to the same location
-        listHead = listTail = listTemp;}
-
-    void addItem(int i);
-    void removeItem();
-    void printList();
-};
-
-int linkedList::nodeCount = 0;
-
-void linkedList::addItem(int i){
-   nodeCount = nodeCount + 1;
-   // Create new node
-   node* newNode = new node;
-   newNode->iData = i;
-   newNode->next = listTail.next;
-   listTail.next = newNode;
-};
-
-void linkedList::removeItem(){};
-
-void linkedList::printList(){cout << "Number of nodes inside the list is " << nodeCount <<endl;
-   node* current;
-   current = listTail.next;
-   cout << "Number of items in the list is ..." << nodeCount << endl;
-   while (current != nullptr){
-      cout << "Data is ..." << current->iData << endl;
-      current = current->next;
-   }
-};
-
+#include "linkedListClasses.cpp"
 int main()
 {
     linkedList list;
@@ -60,5 +7,17 @@ int main()
         list.addItem(i+100);
     }
     list.printList();
+    // Remove one item from the list
+    //list.removeItem();
+    //list.printList();
+    // Remove one item from the list
+    list.removeItem(108);
+    list.printList();
+
+    for (int i=0; i<=10;i++){
+       list.removeItem(i+100);
+    }
+    list.printList();
+
     return 0;
 }
