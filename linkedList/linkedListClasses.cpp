568e589d68ae4c0c3e1e3b792053af9d338f6aad
NICU <nicu_george_florea@yahoo.com>
2019-02-26 22:52:54 +0200

    Updated remove node function to clear bug report.

--- linkedList/linkedListClasses.cpp
+++ linkedList/linkedListClasses.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-
 class node{
 public:
    int iData;
@@ -18,14 +17,13 @@ public:
 private:
     static int nodeCount;
 public:
-
     linkedList(){
        nodeCount = 0;
        // all nodes point to the same location
        listHead.next = &listTail;
        listTail.next = &listHead;
-       }
-
+    }
+    // Function members
     void addItem(int i);
     void removeItem();
     void removeItem(int i);
@@ -37,35 +35,36 @@ int linkedList::nodeCount = 0;
 void linkedList::addItem(int i){
    nodeCount = nodeCount + 1;
    // Create new node
-   node* newNode = new node;
+   node* newNode  = new node;
    newNode->iData = i;
-   newNode->next = listTail.next;
-   listTail.next = newNode;
+   newNode->next  = listTail.next;
+   listTail.next  = newNode;
    // Keep the head pointer to NULL
-   listHead.next = NULL;
+   listHead.next  = NULL;
 };
 
 void linkedList::removeItem(){
    if (nodeCount == 0){cout << "List is empty ..." <<endl;}
-   else{
-
-      node* current;
-      current = listTail.next;
-      listTail.next = current -> next;
-      delete current;}
+   else{node* current;
+        current       = listTail.next;
+        listTail.next = current -> next;
+        delete current;}
    };
 
 void linkedList::removeItem(int i){
    node* current;
+   node* freeNode;
    current = &listTail;
    if (nodeCount == 0){cout << "List is empty ..." <<endl;return;}
    else{
-      while (current != &listHead){
+      while (current->next != &listHead){
          // if value was found
-         if (current -> next -> iData == i){
+         if ((current->next->iData) == i){
             cout<< "Found match in list ... " << endl;
-            delete current -> next;
+            // save the address of the node to be erased
+            freeNode = current->next;
             current -> next = current->next->next;
+            delete freeNode;
             nodeCount = nodeCount - 1;
             return;
          }
--- linkedList/main.cpp
+++ linkedList/main.cpp
@@ -11,13 +11,13 @@ int main()
     //list.removeItem();
     //list.printList();
     // Remove one item from the list
-    list.removeItem(108);
+    list.removeItem(100);
     list.printList();
 
-    for (int i=0; i<=10;i++){
-       list.removeItem(i+100);
-    }
-    list.printList();
+    //for (int i=0; i<=10;i++){
+    //   list.removeItem(i+100);
+    //}
+    //list.printList();
 
     return 0;
 }
