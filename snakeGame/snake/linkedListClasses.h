b312a5f98e49f3845c5ae50d6a20a90c0680fc24
NICU <nicu_george_florea@yahoo.com>
2019-03-24 22:06:27 +0200

    SNAKE:Updated linked list, created snake game area.

--- snakeGame/snake/linkedListClasses.h
+++ snakeGame/snake/linkedListClasses.h
@@ -4,25 +4,43 @@ using namespace std;
 class node{
 public:
    int iData;
+   int x,y;
+   char c;
    node* next;
-   node(){
+   node()
+   {
       iData = 0;
+      x = 0;
+      y = 0;
+      c = ' ';
       next = NULL;
-      cout << "Initializing data ..." << endl;}
+      return;
+    }
 };
 
 class linkedList{
 public:
-    node listHead, listTail;
+    node *listHead, *listTail;
 private:
-    static int nodeCount;
+    int nodeCount;
 public:
-    linkedList(){
+    linkedList()
+	{
        nodeCount = 0;
-       // all nodes point to the same location
-       listHead.next = &listTail;
-       listTail.next = &listHead;
+       listTail  = listHead;
+       return;
     }
+	~linkedList()
+	{
+		cout << "Deleting list ... " << endl;
+		node *freeNode;
+		while(listTail != listHead)
+		{
+			freeNode = listTail;
+			listTail = freeNode->next;
+			delete freeNode;
+		}
+	}
     // Function members
     void addItem(int i);
     void removeItem();
@@ -30,58 +48,73 @@ public:
     void printList();
 };
 
-int linkedList::nodeCount = 0;
-
 void linkedList::addItem(int i){
    nodeCount = nodeCount + 1;
    // Create new node
    node* newNode  = new node;
    newNode->iData = i;
-   newNode->next  = listTail.next;
-   listTail.next  = newNode;
-   // Keep the head pointer to NULL
-   listHead.next  = NULL;
+   newNode->next  = listTail;
+   listTail       = newNode;
 };
 
-void linkedList::removeItem(){
+void linkedList::removeItem()
+{
    if (nodeCount == 0){cout << "List is empty ..." <<endl;}
-   else{node* current;
-        current       = listTail.next;
-        listTail.next = current -> next;
-        delete current;}
-   };
+   else
+   {
+       node* current;
+       current   = listTail;
+       listTail  = current -> next;
+       nodeCount = nodeCount - 1;
+       delete current;
+    }
+};
 
-void linkedList::removeItem(int i){
+void linkedList::removeItem(int i)
+{
    node* current;
    node* freeNode;
-   current = &listTail;
+   current = listTail;
    if (nodeCount == 0){cout << "List is empty ..." <<endl;return;}
-   else{
-      while (current->next != &listHead){
-         // if value was found
-         if ((current->next->iData) == i){
-            cout<< "Found match in list ... " << endl;
-            // save the address of the node to be erased
-            freeNode = current->next;
-            current -> next = current->next->next;
-            delete freeNode;
-            nodeCount = nodeCount - 1;
-            return;
-         }
-         else{
-            current = current -> next;
-         }
-      }
+   else
+   {
+       while (current != listHead)
+	   {
+            // Value found in the first node of the list
+			if ((current->iData) == i)
+			{
+				freeNode = current;
+				listTail = current->next;
+				delete freeNode;
+                nodeCount = nodeCount - 1;
+                return;
+			}
+            else if ((current->next->iData) == i)
+	        {
+             cout<< "Found match in list ... " << endl;
+             // save the address of the node to be erased
+             freeNode = current->next;
+             current -> next = current->next->next;
+             delete freeNode;
+             nodeCount = nodeCount - 1;
+             return;
+            }
+            else
+	        {
+             current = current -> next;
+            }
+       }
       cout <<"Value not found ... "<<endl;
       return;
-   }};
+   }
+};
 
 
 void linkedList::printList(){
-   node* current;
-   current = listTail.next;
+   node *current;
+   current = listTail;
    cout << "Number of items in the list is ..." << nodeCount << endl;
-   while (current != &listHead){
+   while (current != listHead){
       cout << "Data is ..." << current->iData << endl;
       current = current->next;
    }
--- snakeGame/snake/snakeGameClasses.h
+++ snakeGame/snake/snakeGameClasses.h
@@ -1,29 +1,74 @@
 #include "linkedListClasses.h"
-class snakeNode : public node
-{
-public:
-    char printChar;
-    bool moveEn;
-    int x,y;
-	// Constructor definition
-    snakeNode()
-    {
-        x         = 0;
-        y         = 0;
-        moveEn    = false;
-		printChar = '#';
-    }
-};
-
-
+#include<math.h>
 class snakeArea : public linkedList
 {
 public:
-    int size;
+    int w;
+    int h;
+public:
 	// Constructor definition
-	snakeArea()
+	snakeArea(int width, int hight)
 	{
-		size = 4;
+		w = width;
+		h = hight;
+		return;
 	}
+	~snakeArea()
+	{
+		cout << "Deleting area ... " << endl;
+		node *freeNode;
+		while(listTail != listHead)
+		{
+			freeNode = listTail;
+			listTail = freeNode->next;
+			delete freeNode;
+		}
+	}
+
+	void printArea();
+	void setArea();
+
 };
+
+void snakeArea::printArea(){
+   node *current;
+   current = listTail;
+   while (current != listHead){
+	    if (current->x == 0)
+	    {
+           cout << current->c<<endl;
+		}
+		else
+		{
+			cout << current->c;
+		}
+		current = current->next;
+   }
+};
+
+void snakeArea::setArea(){
+    for(int j = 0; j < h; j++)
+    {
+    	for(int i = 0; i < w; i++)
+    	{
+            node* newNode  = new node;
+    		newNode-> x = i;
+    	    newNode-> y = j;
+    	    newNode-> c = '*';
+    	    if ((i==0) || (j==0) || (j==h-1) || (i==w-1))
+    		{
+    			newNode-> c = '*';
+    		}
+    		else
+    		{
+    			newNode-> c = ' ';
+    		}
+            newNode->next  = listTail;
+            listTail       = newNode;
+    	}
+    }
+    return;
+};
+
+
 
