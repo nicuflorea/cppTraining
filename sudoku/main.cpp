1a96906dd711c5f9927fdaac108571bcc3b57b90
NICU <nicu_george_florea@yahoo.com>
2019-04-09 21:50:58 +0300

    sudoku: Sudoku cleanup.

--- sudoku/main.cpp
+++ sudoku/main.cpp
@@ -4,21 +4,9 @@ int main()
 {
     sudokuBoard b1;
     b1.setDefault();
+    cout<<"Solve the following sudoku puzzle ..."<<endl;
     b1.printBoard();
-    b1.findPos(true);
     b1.findSolution();
-
-    //b1.findInCell(8,0,1);
-    //if (b1.findInRow(8,1) == true)
-    //{
-     //  cout << "Value found";
-    //}
-
-
-	//bool b = b1.findInRow(0,5);
-	//bool c = b1.findInCell(1,3,2);
-
-
 
     return 0;
 }
