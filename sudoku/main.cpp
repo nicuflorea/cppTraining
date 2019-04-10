668a5270f8073a5efc93a3dcdff1eebc5a495f58
florea <nicusor.florea@tttech.com>
2019-04-10 09:46:30 +0300

    AERO-123: Removed tabs.

--- sudoku/main.cpp
+++ sudoku/main.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-    sudokuBoard b1;
-    b1.setDefault();
-    cout<<"Solve the following sudoku puzzle ..."<<endl;
-    b1.printBoard();
-    b1.findSolution();
+   sudokuBoard b1;
+   b1.setDefault();
+   cout<<"Solve the following sudoku puzzle ..." <<endl;
+   b1.printBoard();
+   b1.findSolution();
 
-    return 0;
+   return 0;
 }
